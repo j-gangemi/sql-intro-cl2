a7464e8e27f7d6e3b9fab29c59341a26990726c7
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-04-15 15:06:03 +0000

    class 3 exercises

---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+insert into students (
+    first_name
+    , last_name
+    , email
+) VALUES (
+    "Ben"
+    , "Block"
+    , "benjamin.block@kellogg.northwestern.edu"
+)
---
+++ school-8-insert-again.sql
@@ -0,0 +1,8 @@
+insert into students (
+first_name
+, email
+)
+values (
+    "Ben"
+    , "benjamin.block@kellogg.northwestern.edu"
+);
